9b891eb63c3da37da217513a436e72c478408573
libin <libin02@chinatopcredit.com>
2017-02-28 15:31:44 +0800

    Fix: test case

--- test/test_demo.py
+++ test/test_demo.py
@@ -2,10 +2,10 @@
 from sanic import Sanic
 from sanic.response import text
 
-from sanic_limiter import Limiter
+from sanic_limiter import Limiter, get_remote_address
 
 app = Sanic(__name__)
-limiter = Limiter(app, global_limits=['1 per hour', '10 per day'])
+limiter = Limiter(app, global_limits=['1 per hour', '10 per day'], key_func=get_remote_address)
 
 
 @app.route("/t1")
