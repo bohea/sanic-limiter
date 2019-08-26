263b38229365e0bd23653fc0bf95deb3129c5ca0
Christo Goosen <christo.goosen@takectrl.co.za>
2019-08-26 14:38:21 +0200

    Check for remote_adr

--- sanic_limiter/util.py
+++ sanic_limiter/util.py
@@ -8,4 +8,12 @@ def get_remote_address(request):
     :param: request: request object of sanic
     :return: the ip address of given request (or 127.0.0.1 if none found)
     """
-    return request.remote_addr or request.ip
+    # Check if request object has remote_addr attribute set
+    # Seems to break on sanic 19.6.3
+    if hasattr(request, 'remote_addr'):
+        return request.remote_addr
+    elif hasattr(request, '_remote_addr'):
+        return request._remote_addr
+        # return reque
+    else:
+        return request.ip
