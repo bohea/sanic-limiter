4c2f86e041da0ed1cfd11176119a64806275d368
Nicholas Guriev <guriev-ns@ya.ru>
2020-03-05 16:28:35 +0300

    Revert "fix endpoint to view handler"
    This reverts commit e8891ae287e55fe4f6b118c2a0b34e2ae2a08e8d.
    
    Static routes are just named tuples Route that are unnecessarily to
    unpack completely as them content may vary in different Sanic versions.

--- sanic_limiter/extension.py
+++ sanic_limiter/extension.py
@@ -152,9 +152,10 @@ class Limiter(object):
 
     def __check_request_limit(self, request):
         endpoint = request.path or ""
-        view_func, args, kwargs, uri = self.app.router.get(request)
-        if view_func is None:
+        view_handler = self.app.router.routes_static.get(endpoint, None)
+        if view_handler is None:
             return
+        view_func = view_handler.handler
         view_bpname = view_func.__dict__.get('__blueprintname__', None)
         name = ("{}.{}".format(view_func.__module__, view_func.__name__) if view_func else "")
         if (not endpoint
